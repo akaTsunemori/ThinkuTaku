5e82c07d6a3dfcea5738151ba73e70c16b768b29
enz03 <enzozc2@gmail.com>
2023-11-23 12:15:45 -0300

    Create barebones for decision-tree

--- src/rule_parser.py
+++ src/rule_parser.py
@@ -1,5 +1,5 @@
 import re
-
+import decision_tree
 
 def parse_line(line: str):
     """
@@ -25,6 +25,7 @@ def parse_line(line: str):
         line.rsplit(',', 1)[1]]
     line = [i.strip() for i in line]
     cause, symptoms, probability = line
+    symptoms = symptoms.replace("(","").replace(")","")
     return cause, symptoms, probability
 
 
@@ -48,10 +49,39 @@ def parse_file(path):
 
 def main():
     rules = parse_file("static/input.txt")
+
+    # cria a raiz da arvore
+    raiz = decision_tree.Node(is_leaf=False, name="raiz", probability=1)
+
     for rule, symptoms, probability in rules:
+        
+        print("----------------------------")
         print("rule:", rule)
         print("symptoms:", symptoms)
         print("probability:", probability)
+
+    # vou escrever a arvore do exemplo logo nesse arquivo pois acho que faz mais sentido,
+    # ao inves de escrever outro
+
+        symptoms = symptoms.split(",")
+        path_list = []
+        print(symptoms)
+        for sypmtom in symptoms:
+            if sypmtom[0] == "N" or sypmtom[1] == "N":
+                path_list.append(f"!{sypmtom[-1]}")
+            else:
+                path_list.append(sypmtom[-1])
+        print(path_list)
+
+    # pega os indices dos sintomas e regra
+        rule_idx = rule[-1]
+        path_list.append(rule_idx)
+        print(path_list)
+    
+    # adiciona os caminhos a raiz da arvore
+        raiz.insertNode(path_list=path_list, probability=probability, depth=1)
+
+    
 
 
 if __name__ == "__main__":
