a4cccf5e3ca4a2ca615fad412094bf8d45ca4868
akaTsunemori <arthur_07q@hotmail.com>
2023-12-06 11:22:57 -0300

    Add custom styling + Home and game pages + Random background and character images + Initial effort for styling + Remove unnecessary CSS * CSS needs to be studied on resolutions different than 1920x1080 * Most surely the CSS is not consistent as of yet

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from os import listdir
+from random import choice
 from src.decision_tree import DecisionTree
 from src.input_parser import parse_file
 
@@ -9,11 +11,26 @@ PATH = 'static/input.csv'
 rules = parse_file(path=PATH)
 tree = DecisionTree(rules=rules)
 
+# Initialize images
+BACKGROUND_PATH = 'static/img/akihabara'
+backgrounds = listdir(BACKGROUND_PATH)
+CHARACTER_PATH = 'static/img/characters'
+characters = listdir(CHARACTER_PATH)
+
 
 # Routes
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html',
+        image_url=f'{BACKGROUND_PATH}/{choice(backgrounds)}',
+        character=f'{CHARACTER_PATH}/{choice(characters)}')
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html',
+        image_url=f'{BACKGROUND_PATH}/{choice(backgrounds)}',
+        character=f'{CHARACTER_PATH}/{choice(characters)}')
 
 
 # Run the app
