8707c04602cb952d7c0414db5552058058cdad4b
akaTsunemori <arthur_07q@hotmail.com>
2023-12-08 11:24:37 -0300

    Docs

--- src/dialogue_manager.py
+++ src/dialogue_manager.py
@@ -3,6 +3,10 @@ from random import choice, randrange
 
 
 class DialogueManager:
+    '''
+    This is the project's dialogue manager. It manages everything
+    related to dialogues from the characters.
+    '''
     def __init__(self) -> None:
         with open('static/json/symptoms.json', 'r') as file:
             self.comments = load(file)
@@ -22,7 +26,7 @@ class DialogueManager:
 
         Returns:
             Empty string: if the roll fails.
-            Random comment: if the roll succeeds.
+            Random comment + ' ': if the roll succeeds.
         '''
         probability = randrange(100)
         if probability > 20:
@@ -30,6 +34,15 @@ class DialogueManager:
         return choice(self.comments) + ' '
 
     def get_dialogue(self, rule):
+        '''
+        Gets a random dialogue that will question for the presence of [symptom].
+
+        Args:
+            symptom [str]: the symptom which the presence will be asked.
+
+        Returns:
+            question [str]: the properly formatted question.
+        '''
         if rule.startswith('C '):
             raise Exception('Cause passed to symptom function.')
         question = choice(self.questions)
@@ -38,6 +51,16 @@ class DialogueManager:
         return question
 
     def get_success(self, rule):
+        '''
+        Gets a random success message, i.e. the DecisionTree succeeded
+        in finding the cause for the symptoms.
+
+        Args:
+            rule [str]: the name of the character.
+
+        Returns:
+            message [str]: the success message.
+        '''
         if not rule.startswith('C '):
             raise Exception('Success is only for causes. Invalid rule string format.')
         message = choice(self.success)
@@ -45,6 +68,16 @@ class DialogueManager:
         return message
 
     def get_failure(self):
+        '''
+        Returns a random failure message, i.e. the DecisionTree failed to identify the
+        cause of the symptoms.
+
+        Args:
+            None
+
+        Returns:
+            message [str]: the failure message.
+        '''
         return choice(self.failure)
 
 
