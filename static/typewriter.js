3b014de6b38331cb1080dfdd261856005e35e2e0
akaTsunemori <arthur_07q@hotmail.com>
2023-12-07 21:49:16 -0300

    Update typewriter timings

--- static/typewriter.js
+++ static/typewriter.js
@@ -1,21 +1,21 @@
 function printLetterByLetter(destination, message) {
     var i = 0;
-    var speed = 100;
+    var speed = 70;
     function type() {
         document.getElementById(destination).innerHTML += message.charAt(i);
-        if (speed > 50) {
+        if (speed > 70) {
             clearInterval(interval);
-            speed = 50;
+            speed = 70;
             interval = setInterval(type, speed);
         }
-        if (",?!".includes(message.charAt(i))) {
+        if (",?!:;".includes(message.charAt(i))) {
             clearInterval(interval);
-            speed = 750;
+            speed = 600;
             interval = setInterval(type, speed);
         }
         if (message.charAt(i) == ".") {
             clearInterval(interval);
-            speed = 1250;
+            speed = 1200;
             interval = setInterval(type, speed);
         }
         if (i > message.length) {
