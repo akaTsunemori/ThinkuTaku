494c3eb546bb4aaa1b7b6215e0bc0f3b8905aed8
akaTsunemori <arthur_07q@hotmail.com>
2023-12-12 01:27:08 -0300

    Make natural as the default dataset - Checkpoints added - DecisionTree to_dict float precision: .2f

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -472,13 +472,13 @@ class DecisionTree:
         '''
         if not root:
             return
-        root_node = f'{i:06}, {root.data}, {(root.probability*100):.8f}%'
+        root_node = f'{i:06}, {root.data}, {(root.probability*100):.2f}%'
         if root_node not in tree_dict:
             tree_dict[root_node] = dict()
         for child in root.children:
             i+=1
             new_dict = dict()
-            new_node = f'{i:06}, {child.data}, {(child.probability*100):.8f}%'
+            new_node = f'{i:06}, {child.data}, {(child.probability*100):.2f}%'
             tree_dict[root_node][new_node] = new_dict
             self.__to_dict_aux(child, tree_dict[root_node], i)
 
